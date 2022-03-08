28db42efcaec10eec57ec36911a79812bb2e7b21
Eduuard <fabergro@mail.ru>
2022-03-08 22:17:20 +0300

    задание 2.2

---
+++ src/hm2_2.java
@@ -0,0 +1,15 @@
+public class hm2_2 {
+
+    public static void main (String[] args) {
+        int a = 10;
+        int b = 8;
+        int r = 6;
+
+        if (a * a + b * b <= 4 * r * r) {
+            System.out.println("можно закрыть");
+        }
+        else {
+            System.out.println("невозможно закрыть");
+        }
+    }
+}
