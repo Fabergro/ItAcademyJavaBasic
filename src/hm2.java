215be70faac6233af547b6e98cf65186d93a8e09
Eduuard <fabergro@mail.ru>
2022-03-08 22:16:47 +0300

    задание 2

---
+++ src/hm2.java
@@ -0,0 +1,11 @@
+public class hm2 {
+    public static void main (String[] args) {
+
+        System.out.println(getS(1,2));
+
+    }
+    public static int getS (int a, int b) {
+        return a + b + a * b;
+
+    }
+}
