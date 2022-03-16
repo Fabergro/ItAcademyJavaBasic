dcd5fa842d9f2e8baed0eac8037dc088da524c37
Eduuard <fabergro@mail.ru>
2022-03-16 22:20:47 +0300

    задание 4.1

---
+++ src/hm4_1.java
@@ -0,0 +1,9 @@
+public class hm4_1 {
+    public hm4_1() {
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 10, 5, 7, 6, 9, 1, 6, 7};
+        System.out.println(arr[9]);
+    }
+}
